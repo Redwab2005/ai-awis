8d116c90d79ed3464d0fffa869d933e1f4a64f59
ismail adil <redefkinga@gmail.com>
2025-09-24 18:11:09 +0300

    dd signup with  feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,23 +1,72 @@
 const User = require("../model/userModel");
-//this function need alot of modifications like hashing the password
-// and validating the email and password
-//and other improvements
-exports.signup = async (req, res) => {
-  const { email, password, user_name } = req.body;
-  console.log(email, password);
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-  // if there is no email or password or user_name return 400 error
-  if (!email || !password || !user_name) {
-    return res.status(400).send("All fields are required");
+//function to sign token
+const signToken = (id) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return token;
+};
+
+exports.signup = async (req, res) => {
+  try {
+    //user send email ,password and confirm password
+    const { email, password, user_name, confirmPassword } = req.body;
+
+    const newUser = await User.create({
+      email,
+      password,
+      user_name,
+      confirmPassword,
+    });
+    //create token
+    const token = signToken(newUser._id);
+    res.status(200).json({
+      status: "success",
+      message: "User signed up successfully",
+      data: newUser,
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
   }
-  const newUser = await User.create({
-    email,
-    password,
-    user_name,
-  });
-  res.status(200).json({
-    status: "success",
-    message: "User signed up successfully",
-    data: newUser,
-  });
+};
+
+exports.login = async (req, res) => {
+  try {
+    //user send email and password
+    const { email, password } = req.body;
+    //1) check if email and password exist
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Please provide email and password",
+      });
+    }
+    //2) check if user exists && password is correct
+    const user = await User.findOne({ email }).select("+password");
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Incorrect email or password",
+      });
+    }
+    //3) if everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: "success",
+      message: "User logged in successfully",
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
 };
--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,16 +1,30 @@
 const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: [true, "Email already exists"],
+    validate: [validator.isEmail, "Please provide a valid email"],
+    lowercase: true,
   },
   password: {
     type: String,
     required: [true, "Password is required"],
     min: 8,
     select: false, //dont include the password when you reuqest user
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: "Passwords do not match",
+    },
   },
   user_name: {
     type: String,
@@ -29,6 +43,16 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+//middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  //only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  this.confirmPassword = undefined; //do not save confirm password in db
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
